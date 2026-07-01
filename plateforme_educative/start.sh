422f3cf4a26d43f7327a7f89e0fbc937e7af176a
jetskos <yussouf2002p@gmail.com>
2026-07-01 07:34:32 +0100

    Fix Celery SQLite deadlock by using pool=solo

--- plateforme_educative/start.sh
+++ plateforme_educative/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Démarrer le worker Celery en arrière-plan avec 1 seul thread pour économiser la RAM
-celery -A core worker -l info --concurrency=1 &
+# Démarrer le worker Celery en arrière-plan avec le pool solo pour éviter les deadlocks de fork ChromaDB
+celery -A core worker -l info --pool=solo &
 
 # Démarrer le serveur web Daphne au premier plan
 daphne -b 0.0.0.0 -p ${PORT:-8000} core.asgi:application
