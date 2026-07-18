3df3de7a77dfe299723593c9e0aa79bb662f1035
jetskos <yussouf2002p@gmail.com>
2026-07-18 17:08:31 +0100

    update for deployement server ecole

--- plateforme_educative/start.sh
+++ plateforme_educative/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Démarrer le worker Celery en arrière-plan avec le pool solo pour éviter les deadlocks de fork ChromaDB
-celery -A core worker -l info --pool=solo &
-
-# Démarrer le serveur web Daphne au premier plan
+# Démarrer le serveur web Daphne au premier plan (serveur de production)
 daphne -b 0.0.0.0 -p ${PORT:-8000} core.asgi:application
